ffc205fac4f07deeca57f8df82291a445cf048c9
Brad Beltowski <bradbeltowskigmail.com>
2026-01-21 19:38:36 -0500

    Handling Sessions on the backend.

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const authRoutes = require("./authRoutes");
 const sessionRoutes = require("./sessionRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
 // localhost:3000/api/v1/auth
 router.use("/auth", authRoutes);
 router.use("/session", sessionRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
